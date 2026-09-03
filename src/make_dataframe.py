a0071219e4f35baf3decaf1ce9a1cd5d03361ac7
niev <nielson-voelk@web.de>
2026-09-03 15:03:30 +0200

    added script for deleting the environment

--- src/make_dataframe.py
+++ src/make_dataframe.py
@@ -2,8 +2,9 @@ from pathlib import Path
 import pandas as pd
 
 current_dir = Path(__file__).resolve().parent
-audio_file_path = current_dir.parent / "data/mp3/kicklherbert_7409992801452182817.mp3"
 metadata_file_path = current_dir.parent / "data/video_metadata.jsonl"
+account_person_mapping_path = current_dir.parent / "data/Deutsche Politik auf Tik Tok - Eine Sammlung von Martin Fuchs @wahl_beobachter.xlsx"
+out_path = current_dir.parent / "data/video_metadata.csv"
 
 #metadata_df = pd.DataFrame(columns=["id", "username", "view_count", "like_count", "share_count", "comment_count", "create_time", "video_duration"])
 
@@ -24,3 +25,22 @@ metadata_df.rename(columns=renaming, inplace=True)
 print(metadata_df.head())
 
 #CONTINUE: merge with account_person_mapping to get the party affiliations into the df
+account_person_mapping_df = pd.read_excel(account_person_mapping_path, skiprows=1)
+
+account_person_mapping_df = account_person_mapping_df[~account_person_mapping_df["Tik Tok-Profil"].isna()]#only keep the rows where we have profiles
+
+def extract_name(profile_url:str) -> str:
+    name = profile_url.split("@")[-1].rstrip()
+    return name
+
+account_person_mapping_df["profile_name"] = account_person_mapping_df["Tik Tok-Profil"].apply(extract_name)
+
+account_person_mapping_df = account_person_mapping_df[["Partei", "profile_name"]]
+
+output_df = pd.merge(metadata_df, account_person_mapping_df, left_on="UserID", right_on="profile_name", how="left")
+output_df = output_df.drop(columns=["profile_name"])#don't need to keep duplicate column
+output_df.rename(columns={"Partei":"party"}, inplace=True)
+
+print(output_df.head())
+
+output_df.to_csv(out_path, index=False)
