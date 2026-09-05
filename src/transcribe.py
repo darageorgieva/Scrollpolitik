931a249824a63dbb78113440f506db9f20ca8e44
niev <nielson-voelk@web.de>
2026-09-05 17:37:58 +0200

    better documentation for transcribe.py

--- src/transcribe.py
+++ src/transcribe.py
@@ -1,24 +1,61 @@
+"""
+Video transcription Solovev et al. (2026) (Methods, Sect 3.1)
+
+Reproduces the paper's video transcription method using the
+openai/whisper model.
+
+Prerequisites:
+Requires data/video_metadata_media_type.csv" to be present,
+which can be generated with: python determine_media_type.py
+
+Remarks:
+Will skip transcription for all media items where
+media_type != video (as specified in
+data/metadata_media_type.csv).
+
+Usage:
+python transcribe.py --metadat video_metadata_media_type.csv --out videos_transcribed.csv --model turbo --chkpt_itv 100
+python transcribe.py --model tiny --limit 10
+
+Requires: pip install torch, pandas, openai-whisper
+
+"""
+import argparse
 import whisper
 from pathlib import Path
 import pandas as pd
 
-CHECKPOINT_INTERVAL = 2
+ap = argparse.ArgumentParser()
+
+ap.add_argument("--metadat", default="video_metadata_media_type.csv")
+ap.add_argument("--out", default="videos_transcribed.csv")
+ap.add_argument("--limit", type=int, default=None,
+                help="only transcribe the first N videos to test functionality")
+ap.add_argument("--model", default="turbo",
+                help="Available values: tiny, base, small, medium, large, turbo")
+ap.add_argument("--chkpt_itv", type=int, default=100,
+                help="interval for checkpoints of all current transcriptions.")
+
+args = ap.parse_args()
+
+CHECKPOINT_INTERVAL = args.chkpt_itv
 
 current_dir = Path(__file__).resolve().parent
-path_df = current_dir.parent / "data/video_metadata_media_type.csv"#the df containing the metadata and party affiliation
-path_df_checkpoint = current_dir.parent / "data/videos_transcribed.csv"
+path_df = current_dir.parent / f"data/{args.metadat}"#the df containing the video metadata
+out_path = current_dir.parent / f"data/{args.out}"#file where output will be saved
 
 df = pd.read_csv(path_df)
 
 df["text"] = ""#add empty column for transcriptions
 
-#for debugging: only do the first 10 videos
-df = df.head(10)
+#if a limit for debugging is enabled: only process the first N rows
+if args.limit:
+    df = df.head(limit)
 
 print(df.head())
 
 #load the whisper model
-model = whisper.load_model("turbo")
+model = whisper.load_model(args.model)
 
 bad_video_ids = []#list that will contain the ids of all the videos that failed to transcribe
 
@@ -37,10 +74,10 @@ for i, vid in enumerate(df.itertuples()):
 
     #save a backup from time to time
     if vid.Index % CHECKPOINT_INTERVAL == 0:
-        df.to_csv(str(current_dir.parent / f"data/videos_transcribed_checkpoint_{vid.Index}.csv"))
+        df.to_csv(str(current_dir.parent / f"data/videos_transcribed_checkpoint_{i}.csv"))
 
 #save final transcriptions
-df.to_csv(str(current_dir.parent / f"data/videos_transcribed.csv"))
+df.to_csv(out_path)
 
 #save list of video ids that couldn't be transcribed:
 if len(bad_video_ids):
