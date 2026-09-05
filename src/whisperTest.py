619930b22795e11bd09c7cfc1d73fb12b2187a81
niev <nielson-voelk@web.de>
2026-09-05 22:23:55 +0200

    added job for installing some required packages on hpc

--- src/whisperTest.py
+++
@@ -1,9 +0,0 @@
-import whisper
-from pathlib import Path
-
-current_dir = Path(__file__).resolve().parent
-audio_file_path = current_dir.parent / "data/mp3/kicklherbert_7409992801452182817.mp3"
-
-model = whisper.load_model("turbo")
-result = model.transcribe(str(audio_file_path))
-print(result["text"])
